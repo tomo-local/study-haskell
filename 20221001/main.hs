dc31db7f09d57444d3c2c8ff52c0db53e0919c53
Togashi-sys <t.togashi@strategit.jp>
2022-10-10 02:00:08 +0900

    :+1: 20221001勉強した内容

---
+++ 20221001/main.hs
@@ -0,0 +1,12 @@
+import Data.IORef
+
+main = do
+  name <- getName " "
+  putStrLn name
+
+getName str = do
+  putStrLn "姓: "
+  lastName <- getLine
+  putStrLn "名: "
+  firstName <- getLine
+  return (lastName ++ str ++ firstName)
