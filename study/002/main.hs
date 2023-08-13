b8ea905bb6f5c321bab35b1b1914ce58061ddad2
tomo-local <shinsi4545@gmail.com>
2023-08-14 05:04:00 +0900

    add fizzBuzz

---
+++ study/002/main.hs
@@ -0,0 +1,27 @@
+
+import Text.Printf (printf)
+import Language.Haskell.TH.Ppr (pprString)
+
+printFizzBuzz :: Int -> IO ()
+printFizzBuzz n = mapM_ (\(i, result) -> printf "[%0*d]: %s" maxDigits i result >> putStrLn "") numberedResults
+    where
+      -- fizzBuzz results
+        numberedResults = zip [1..n] (map fizzBuzz [1..n])
+      -- maximum number of digits
+        maxDigits = length (show n)
+
+fizzBuzz :: Int -> String
+fizzBuzz n
+  | n `mod` 15 == 0 = "FizzBuzz"
+  | n `mod` 3 == 0  = "Fizz"
+  | n `mod` 5 == 0  = "Buzz"
+  | otherwise = " "
+
+main :: IO ()
+main = do
+ putStrLn "How long? :"
+ num :: Int <- readLn
+ putStrLn "================"
+ printFizzBuzz num
+ putStrLn "================"
+
