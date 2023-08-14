5c7853b1ae5ed02ffabdd62f2228dfc91e38a81d
tomo-local <shinsi4545@gmail.com>
2023-08-15 00:42:01 +0900

    fix indent

--- study/002/countdown.hs
+++ study/002/countdown.hs
@@ -3,16 +3,16 @@ import Control.Concurrent (threadDelay)
 -- recursion
 countdown :: Int -> IO ()
 countdown n
-    | n <= 0 = putStrLn "💣 Boom! 💣"
-    | otherwise = do
-        print n
-        threadDelay (1000 * 1000)
-        countdown (n - 1)
+  | n <= 0 = putStrLn "💣 Boom! 💣"
+  | otherwise = do
+      print n
+      threadDelay (1000 * 1000)
+      countdown (n - 1)
 
 main :: IO ()
 main = do
-    putStrLn "Enter a starting seconds:"
-    num :: Int <- readLn
+  putStrLn "Enter a starting seconds:"
+  num :: Int <- readLn
 
-    putStrLn "Starting countdown..."
-    countdown num
+  putStrLn "Starting countdown..."
+  countdown num
--- study/002/fizzBuzz.hs
+++ study/002/fizzBuzz.hs
@@ -1,20 +1,19 @@
-
-import Text.Printf (printf)
 import Language.Haskell.TH.Ppr (pprString)
+import Text.Printf (printf)
 
 printFizzBuzz :: Int -> IO ()
 printFizzBuzz n = mapM_ (\(i, result) -> printf "[%0*d]: %s" maxDigits i result >> putStrLn "") numberedResults
-    where
-      -- fizzBuzz results
-        numberedResults = zip [1..n] (map fizzBuzz [1..n])
-      -- maximum number of digits
-        maxDigits = length (show n)
+  where
+    -- fizzBuzz results
+    numberedResults = zip [1 .. n] (map fizzBuzz [1 .. n])
+    -- maximum number of digits
+    maxDigits = length (show n)
 
 fizzBuzz :: Int -> String
 fizzBuzz n
   | n `mod` 15 == 0 = "FizzBuzz"
-  | n `mod` 3 == 0  = "Fizz"
-  | n `mod` 5 == 0  = "Buzz"
+  | n `mod` 3 == 0 = "Fizz"
+  | n `mod` 5 == 0 = "Buzz"
   | otherwise = " "
 
 main :: IO ()
@@ -24,4 +23,3 @@ main = do
   putStrLn "================"
   printFizzBuzz num
   putStrLn "================"
-
