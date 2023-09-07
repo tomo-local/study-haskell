07c703521e1ac0def819b418b5c1e5a233de248c
tomo-local <shinsi4545@gmail.com>
2023-09-07 13:32:29 +0900

    add comment

--- study/003/List/list.hs
+++ study/003/List/list.hs
@@ -15,6 +15,11 @@ main = do
   currentPath <- getCurrentDirectory
   currentContents <- getDirectoryContents currentPath
   let filteredContents =
+        {-
+          TODO:　-Aの場合を実装する
+          -Aの場合は、. と .. を除いた、すべてのファイルを出力する 下記のURLに使用がある。
+          https://atmarkit.itmedia.co.jp/ait/articles/1606/27/news018.html
+        -}
         if "-a" `elem` args
           then currentContents
           else filterNonSpecial currentContents
