7ecda043ff47d3be72e73cec01fa0a819c05849e
tomo-local <shinsi4545@gmail.com>
2025-01-25 17:56:22 +0900

    add 新しい関数addを追加し、main関数でその出力を表示

--- study/004/function/main.hs
+++ study/004/function/main.hs
@@ -1,5 +1,15 @@
 f x = x + 1
+add x y = x + y
+
 main = do
   print (f 4)
   print $ f 10
 
+  print $ add 1 2
+  print $ 3 `add` 4
+
+  print $ 1 + 2
+  print $ (+) 1 2
+
+  print $ 1 `add` 2
+  print $ add 1 (f 2)
