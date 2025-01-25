bc20b67ca28b8f0ed956e926d28f2b1b7e70a09c
tomo-local <shinsi4545@gmail.com>
2025-01-25 23:57:48 +0900

    main関数の出力方法を修正し、条件に基づいて文字列を表示するように変更

--- study/004/if/main.hs
+++ study/004/if/main.hs
@@ -1,4 +1,5 @@
 a = 1
 
 main = do
-  if a == 1 then putStrLn "a is 1" else putStrLn "a is not 1"
+  -- if a == 1 then putStrLn "a is 1" else putStrLn "a is not 1"
+  print $ if a == 1 then "a is 1" else "a is not 1"
