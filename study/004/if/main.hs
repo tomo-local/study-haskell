39f25f6a6030522f687228eef34fad71fd075425
tomo-local <shinsi4545@gmail.com>
2025-01-26 00:04:15 +0900

    add 新しい関数fとf_2を追加し、main関数でその出力を表示

--- study/004/if/main.hs
+++ study/004/if/main.hs
@@ -1,5 +1,13 @@
-a = 1
+f a = if a == 1 then "a is 1" else "a is not 1"
+
+f_2 0 = "zero"
+f_2 _ = "other"
 
 main = do
   -- if a == 1 then putStrLn "a is 1" else putStrLn "a is not 1"
-  print $ if a == 1 then "a is 1" else "a is not 1"
+  -- print $ if a == 1 then "a is 1" else "a is not 1"
+  print $ f 1
+  print $ f 0
+
+  print $ f_2 0
+  print $ f_2 1
